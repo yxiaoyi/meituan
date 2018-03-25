647b1727b82409f6edd187899fa6d2c4c536052f
“yhy” <ychishuo@163.com>
2018-03-25 10:00:09 +0800

    初始化项目

---
+++ MeiTuan/MeiTuan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeiTuan
+//
+//  Created by 易荟云 on 2018/3/25.
+//  Copyright © 2018年 易荟云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MeiTuan/MeiTuan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeiTuan
+//
+//  Created by 易荟云 on 2018/3/25.
+//  Copyright © 2018年 易荟云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
