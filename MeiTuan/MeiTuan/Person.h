726fe058ef7f7399de2b086a90d8c9a8c87f6be4
“yhy” <ychishuo@163.com>
2018-03-25 10:02:11 +0800

    创建person类

---
+++ MeiTuan/MeiTuan/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  MeiTuan
+//
+//  Created by 易荟云 on 2018/3/25.
+//  Copyright © 2018年 易荟云. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ MeiTuan/MeiTuan/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  MeiTuan
+//
+//  Created by 易荟云 on 2018/3/25.
+//  Copyright © 2018年 易荟云. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
